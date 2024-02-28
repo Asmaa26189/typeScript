6e62ba29ca3b03b83a7223c05006501010c01a50
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-28 10:28:18 +0200

    28-2-2024

--- test.ts
+++ test.ts
@@ -1,11 +1,13 @@
 let message: string = 'Hello World';
 console.log(message);
-
+console.log(typeof window);
 if (typeof window !== 'undefined') {
-    alert('You are on the browser')
+    alert('You are on the browser');
+    console.log('can use alert()');
     //can use alert()
   } else {
-    console.log('You are on the server')
+    console.log('You are on the server');
+    console.log('can\'t use alert()');
     //can't use alert()
   }
   
