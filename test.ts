f470a87b60ead35321d3279a0c71edfd6ce828bb
Asmaa26189 <engasmaahesham@gmail.com>
2024-02-28 10:24:49 +0200

    28-2-2024

---
+++ test.ts
@@ -0,0 +1,11 @@
+let message: string = 'Hello World';
+console.log(message);
+
+if (typeof window !== 'undefined') {
+    alert('You are on the browser')
+    //can use alert()
+  } else {
+    console.log('You are on the server')
+    //can't use alert()
+  }
+  
